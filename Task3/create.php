359a1cd586f0842db279499708eb262995a9a811
Vũ Việt Anh <73330605+vabi1232@users.noreply.github.com>
2021-08-03 22:31:49 +0700

    Add files via upload

---
+++ Task3/create.php
@@ -0,0 +1,101 @@
+<?php
+require_once 'config.php';
+
+$name = $address = $salary = "";
+$name_err = $address_err = $salary_err = "";
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $input_name = trim($_POST["name"]);
+    if (empty($input_name)){
+        $name_err="Enter a name";
+    }elseif (!filter_var(trim($_POST["name"]),FILTER_VALIDATE_REGEXP ,
+        array("options"=>array("regexp"=>"/^[a-zA-Z'-.\s]+$/")))){
+        $name_err = 'Enter a valid name';
+    }else{
+        $name = $input_name;
+    }
+
+    $input_address = trim($_POST["address"]);
+    if (empty($input_address)){
+        $address_err="Enter an address";
+    }else {
+        $address = $input_address;
+    }
+
+    $input_salary = trim($_POST["salary"]);
+    if (empty($input_salary)){
+        $salary_err="Enter the salary amount.";
+    }elseif (!ctype_digit($input_salary)){
+        $salary_err ="Enter a positive integer value";
+    }else{
+        $salary = $input_salary;
+    }
+    if (empty($name_err)&& empty($address_err) && empty($salary_err)){
+        $sql = "INSERT INTO employee (name, address, salary)VALUE (?,?,?)";
+        if ($stmt = mysqli_prepare($link,$sql)){
+            mysqli_stmt_bind_param($stmt,"ssd",$param_name,$param_address,$param_salary);
+            $param_name = $name;
+            $param_address = $address;
+            $param_salary = $salary;
+
+            if (mysqli_stmt_execute($stmt)){
+                header("location:index.php");
+                exit();
+            }else{
+                echo "Something went wrong.Please try again later";
+            }
+        }
+        mysqli_stmt_close($stmt);
+
+    }
+    mysqli_close($link);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width:  500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+<div class="wrapper">
+    <div class="container_fluid">
+        <div class="row">
+            <div class="page-header">
+                <h2>Create Record</h2>
+            </div>
+            <p>Please fill this and submit to add employee record the database</p>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
+                <div class="form-group <?php echo (!empty($name_err))? 'has-error':''; ?>">
+                    <label>Name</label>
+                    <input type="text" name="name" class="form-control" value="">
+                    <span class="help-block"><?php echo $name_err; ?></span>
+                </div>
+
+                <div class="form-group <?php echo (!empty($address_err))? 'has-error':''; ?>">
+                    <label>Address</label>
+                    <input type="text" name="address" class="form-control" value="">
+                    <span class="help-block"><?php echo $address_err; ?></span>
+                </div>
+
+                <div class="form-group <?php echo (!empty($salary_err))? 'has-error':''; ?>">
+                    <label>Salary</label>
+                    <input type="text" name="salary" class="form-control" value="">
+                    <span class="help-block"><?php echo $salary_err; ?></span>
+                </div>
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <a href="index.php" class="btn btn-default">Cancel</a>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
